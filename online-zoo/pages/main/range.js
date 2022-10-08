7872a6d3d725b378d802805e2608e62c554779ce
LineCoran <vppisya@gmail.com>
2022-10-08 17:14:34 +0300

    feat: testi done 100%

--- online-zoo/pages/main/range.js
+++ online-zoo/pages/main/range.js
@@ -3,7 +3,8 @@
     const slider = document.getElementById('review_slider');
     let currentPosition = 0;
     let currentValue = range.value;
-    console.log(currentValue)
+   
+
     range.addEventListener('change', function(){
         let nextValue = range.value;
         moveSlider(nextValue-currentValue);
@@ -13,5 +14,17 @@
     function moveSlider(num) {
         currentPosition -= num*(parseFloat(getComputedStyle(slider.firstElementChild).width)+parseFloat(getComputedStyle(slider).columnGap));
         slider.style.transform = `translateX(${currentPosition}px)`;
+        window.addEventListener('resize', startSetting)
+    }
+
+    function startSetting() {
+        console.log('hello')
+        if(range.value!==0){
+            range.value = 0;
+            currentValue = 0
+            currentPosition = 0;
+            moveSlider(0);
+        }
+        window.removeEventListener('resize', startSetting)
     }
 }())
