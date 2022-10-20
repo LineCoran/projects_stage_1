e7de66319ad0ed0d9fbfacb0d95e85244391b05d
Asus <vppisya@gmail.com>
2022-10-20 22:14:36 +0300

    feat: size, styles, adaptive, setting 70%

--- codejam-the-gem-puzzle/script.js
+++ codejam-the-gem-puzzle/script.js
@@ -1,6 +1,73 @@
-const containerNode = document.getElementById('PuzzleNode');
-const itemNodes = Array.from(containerNode.querySelectorAll('.puzzle__item'));
-const COUNT_ITEM = 16;
+const body = document.getElementById('body');
+let setting = {
+    size: 4,
+    sound: true,
+}
+
+function createPuzzle() {
+    let container = document.createElement('div');
+    container.classList.add('container');
+
+    let puzzleNode = document.createElement('div');
+    puzzleNode.id = "PuzzleNode";
+    puzzleNode.classList = "puzzle";
+    for (let i = 1; i <= setting.size**2; i++) {
+        let puzzle = document.createElement('span');
+        if (i === setting.size**2) {
+            puzzle.id = "last__item"   
+        }
+        puzzle.classList = ('puzzle__item');
+        puzzle.setAttribute(`data-id`, `${i}`);
+        puzzle.innerHTML = `${i}`;
+        puzzle.style.height = `${100/setting.size}%`
+        puzzle.style.width = `${100/setting.size}%`
+        puzzleNode.append(puzzle);
+
+        
+    }
+    body.append(container)
+    container.append(puzzleNode);
+ 
+}
+
+createPuzzle();
+
+
+const chooseSizeButtons = document.querySelectorAll('.size__button');
+chooseSizeButtons.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        setting.size = Number(event.target.id.split('size-')[1]);
+        body.lastElementChild.remove();
+        createPuzzle()
+        containerNode = document.getElementById('PuzzleNode');
+        itemNodes = Array.from(containerNode.querySelectorAll('.puzzle__item'));
+        COUNT_ITEM = setting.size**2;
+        matrix = getMatrix(itemNodes.map(item => Number(item.dataset.id)));
+        setPositionItems(matrix)
+        unvisibleItem = setting.size**2;
+
+        containerNode.addEventListener('click', (event)=> {
+            const itemNode = event.target.closest('span');
+            if (!itemNode) {
+                return
+            }
+            const itemNumber = Number(itemNode.dataset.id);
+            const clickedItemCoordinate = findCoordinateByItemId(matrix, itemNumber);
+            const unvisibleItemCoordinate = findCoordinateByItemId(matrix, unvisibleItem);
+            const canWeSwap = checkedPossibilityToSwap(clickedItemCoordinate, unvisibleItemCoordinate);
+        
+            if (canWeSwap) {
+                swapItems(clickedItemCoordinate, unvisibleItemCoordinate, matrix)
+            }
+            
+        })
+    })
+});
+
+let containerNode = document.getElementById('PuzzleNode');
+let itemNodes = Array.from(containerNode.querySelectorAll('.puzzle__item'));
+let COUNT_ITEM = setting.size**2;
+
 
 
 
@@ -8,19 +75,23 @@ let matrix = getMatrix(
     itemNodes.map(item => Number(item.dataset.id))
 )
 
-const wonMatrix = new Array(16).fill(0).map((item, i) => i+1)
+const wonMatrix = new Array(setting.size**2).fill(0).map((item, i) => i+1)
 
 
 setPositionItems(matrix) 
 
 /* set position */
 function getMatrix(arr) {
-    let matrix = [[], [], [], []];
+    let matrix = [];
+    for (let i = 0; i < setting.size; i ++) {
+        matrix.push([])
+    }
+    
     let x = 0;
     let y = 0;
 
     for (let i = 0; i<arr.length; i++) {
-        if (x == 4) {
+        if (x == setting.size) {
             y++;
             x = 0;
         }
@@ -42,7 +113,6 @@ function setPositionItems(matrix) {
 
 function setPosititonStyleCss(item, x, y) {
     const itemWidth = 100;
-    item.style.background = "yellow";
     item.style.transform = `translate(${x*itemWidth}%, ${y*itemWidth}%)`;
 }
 
@@ -59,7 +129,7 @@ shuffleButton.addEventListener('click', () => shuffleItems());
 
 /* Change position by click */
 
-const unvisibleItem = 16;
+let unvisibleItem = setting.size**2;
 containerNode.addEventListener('click', (event)=> {
     const itemNode = event.target.closest('span');
     if (!itemNode) {
@@ -150,7 +220,7 @@ window.addEventListener('keydown', (event) => {
 })
 
 function checkedPossibilityToSwapByKey(coord) {
-    if (coord.x>3 || coord.y>3 || coord.x<0 || coord.y <0) {
+    if (coord.x>setting.size-1 || coord.y>setting.size-1 || coord.x<0 || coord.y <0) {
         return false
     } else {
         return true;
