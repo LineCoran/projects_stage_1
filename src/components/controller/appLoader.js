0e41a03b5db563a7f390d77e515c0339ddb0aaa3
LineCoran <vppisya@gmail.com>
2022-12-06 21:30:19 +0300

    feat: add API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '0c3bd48eb91d4f5f8c68f76c99ddcf48', // получите свой ключ https://newsapi.org/
         });
     }
 }
